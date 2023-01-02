13b30ecc1280cf178b49faa4528ab3918efb94b1
Mahdi Mohamed <14154030+mvhdi@users.noreply.github.com>
2023-01-01 18:57:57 -0600

    setup app for calculating fractional shares

---
+++ fractional_shares.js
@@ -0,0 +1,12 @@
+// this app takes in the dollar amount that a user wants to invest in,
+// a list of tickers and the percentage of the position size
+// and out puts the fractional number of shares at the current price point for each postion.
+
+
+// generate the html form
+
+// event listners to form submission to correct errors
+
+// on form submission get live data, and generate table with generated info.
+
+
